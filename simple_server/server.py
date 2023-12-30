2e1753c14d4c129cfb1f1206eb496acde40e4f95
Lorenzo Peve <71228480+LorenzoPeve@users.noreply.github.com>
2023-12-30 17:39:54 -0600

    Update server.py

--- simple_server/server.py
+++ simple_server/server.py
@@ -1,7 +1,7 @@
 from socket import *
 
-IP = '172.31.36.202'
-PORT = 12000
+IP = '172.31.36.202' # Your EC2 instance private IP
+PORT = 12000 # Your port of choice
 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind((IP, PORT))

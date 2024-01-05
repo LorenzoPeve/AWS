2b50df884f115d7085374f97665bacdf6b634e37
Lorenzo Peve <lpeve01@gmail.com>
2024-01-04 22:58:09 -0600

    wip

--- simple_webserver/server.py
+++ simple_webserver/server.py
@@ -2,13 +2,14 @@ import os
 import socket
 from datetime import datetime
 
-server_ip = "172.31.25.238"
+server_ip = "172.31.30.247"
 port = 80
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((server_ip, port))
 server.listen()
 print(f"Listening on {server_ip}:{port}")
+
 
 def handle_request(s: str):
     
@@ -33,7 +34,6 @@ while True:
     print(f'Accepted connection from {cl_addr} at {datetime.now().strftime("%H:%M:%S")}')
 
     request = conn_socket.recv(1024).decode()
-    print(request)
     response = handle_request(request)
     conn_socket.send(response.encode())
     conn_socket.close()
